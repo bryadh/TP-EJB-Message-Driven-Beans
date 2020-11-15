a7458ae765fb7386402bb7880742a09f43cbe26f
bryadh <ryadh.belghanem@gmail.com>
2020-11-15 12:16:45 +0100

    rendu final

--- src/main/java/converter/IConverter.java
+++ src/main/java/converter/IConverter.java
@@ -1,11 +1,15 @@
 package converter;
 
-import javax.ejb.EJB;
-import java.net.MalformedURLException;
 
-@EJB
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.Map;
+
 public interface IConverter {
 
     public double euroToOtherCurrency(double amount, String currencyCode);
-
+    public List<String> getAvailableCurrencies();
+    public Map<Monnaie,Double> euroToOtherCurrencies(double amount);
 }
